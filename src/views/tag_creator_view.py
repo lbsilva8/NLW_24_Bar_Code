01156181f08e8ab18d7fcde1d25240bc6ffb1869
Lorena <lorenaborgessilva@gmail.com>
2024-02-10 21:23:00 -0300

    isolando biblioteca externa estruturando controller

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,13 +1,17 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controller.tag_creator_controller import TagCreatorController
 
 class TagCreatorView :
     """
     interação com http
     """
     def validade_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        tag_creator_controller = TagCreatorController()
+
         body = http_request.body
         product_code = body["product_code"]
-        print(product_code)
 
-        return HttpResponse(status_code=200, body={"hello": "world"})
+        formatted_response = tag_creator_controller.create(product_code)
+
+        return HttpResponse(status_code=200, body=formatted_response)
